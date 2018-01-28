b4309fd7b524238a6483268f2edc40b0fbceda0f
as <as.utf8@gmail.com>
2018-01-28 00:16:09 -0800

    font: goregular: error check on init

--- goregular/goregular.go
+++ goregular/goregular.go
@@ -1,8 +1,16 @@
 package goregular
 
 import (
+	"log"
+
 	"github.com/golang/freetype/truetype"
 	. "golang.org/x/image/font/gofont/goregular"
 )
 
-var Font, _ = truetype.Parse(TTF)
+var Font, err = truetype.Parse(TTF)
+
+func init() {
+	if err != nil {
+		log.Fatalln("goregular", err)
+	}
+}
